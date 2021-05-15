3873921f67a99a6bc821c428adeeba6bbf450f61
De Wet Blomerus <dewet@blomerus.org>
2021-05-15 05:56:34 -0400

    mix new whichsup --sup

---
+++ lib/whichsup.ex
@@ -0,0 +1,18 @@
+defmodule Whichsup do
+  @moduledoc """
+  Documentation for `Whichsup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Whichsup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/whichsup_test.exs
@@ -0,0 +1,8 @@
+defmodule WhichsupTest do
+  use ExUnit.Case
+  doctest Whichsup
+
+  test "greets the world" do
+    assert Whichsup.hello() == :world
+  end
+end
